a1a51b03e61ddd11f3321dc643d2ca2a3ab46f5b
Adnan <adnansihab25@gmail.com>
2024-12-27 03:59:03 +0600

    Account Activation Process (Verification Link) 1 done

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,37 @@ from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 
+#Verification Email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = RegistrationForm(request.POST)
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             phone_number = form.cleaned_data['phone_number']
+#             email = form.cleaned_data['email']
+#             password = form.cleaned_data['password']
+#             username = email.split("@")[0]
+#             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+#             user.phone_number = phone_number
+#             user.is_active = True
+#             user.save()
+#             messages.success(request, 'Registration Successful')
+#             return redirect('register')
+#     else:
+#         form = RegistrationForm()
+    
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'accounts/register.html', context)  # This line should be outside the if/else block
 
 def register(request):
     if request.method == 'POST':
@@ -17,18 +48,28 @@ def register(request):
             username = email.split("@")[0]
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
-            user.is_active = True
             user.save()
-            messages.success(request, 'Registration Successful')
-            return redirect('register')
+
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            # messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email address [rathan.kumar@gmail.com]. Please verify it.')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
-    
     context = {
         'form': form,
     }
-    return render(request, 'accounts/register.html', context)  # This line should be outside the if/else block
-
+    return render(request, 'accounts/register.html', context)
 
 def login(request):
     if request.method == 'POST':
@@ -51,6 +92,9 @@ def logout(request):
     messages.success(request, 'You are logged out.')
     return redirect('login')
 
+
+def activate(request):
+    return
 
 # def activate(request, uidb64, token):
 #     try:
